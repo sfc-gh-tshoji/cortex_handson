6eb104d1edf9db2cf60d836e50c816d1eb8a4576
Takuya Shoji <takuya.shoji@snowflake.com>
2026-07-21 12:16:03 +0900

    Update 04_create_semantic_view_agent.sql

--- sql/04_create_semantic_view_agent.sql
+++ sql/04_create_semantic_view_agent.sql
@@ -3,7 +3,7 @@
 -- ================================================================================
 
 USE ROLE SYSADMIN;
-USE WAREHOUSE DEMO_WH;
+USE WAREHOUSE SNOWFLAKE_LEARNING_WH;
 USE SCHEMA DEMO_DB.LEASING;
 
 -- ============================================================
